bd72de36b174e221d30282176d14f862d93631f4
Sumit Kumar Mridha <3sumit5@gmail.com>
2019-03-14 19:57:10 +0530

    Updates

---
+++ precache-manifest.06fbb7ca7a2a3b8998ca53f853e98e2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a77a7860f00f652a4fcb",
+    "url": "/mini-phone-directory/static/js/runtime~main.0bb86903.js"
+  },
+  {
+    "revision": "a8b368b31bc9dbf90228",
+    "url": "/mini-phone-directory/static/js/main.09193f77.chunk.js"
+  },
+  {
+    "revision": "1bfbde3508fa689b2b0a",
+    "url": "/mini-phone-directory/static/js/2.e1311f5e.chunk.js"
+  },
+  {
+    "revision": "a8b368b31bc9dbf90228",
+    "url": "/mini-phone-directory/static/css/main.4e3e2beb.chunk.css"
+  },
+  {
+    "revision": "06ac27137f05bb4be48a64f5b1313e53",
+    "url": "/mini-phone-directory/index.html"
+  }
+];
